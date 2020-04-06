086df869148c15c2ff0afb451c56b605e4232079
toshinomi <tatsuya.oshinomi.0828@gmail.com>
2020-04-06 18:18:00 +0900

    Update

--- src/module/common.rs
+++ src/module/common.rs
@@ -1,3 +1,47 @@
+pub enum PixelKind {
+    Red,
+    Green,
+    Blue,
+    Alpha,
+} 
+
+pub struct Pixel<T> {
+    red:    T,
+    green:  T,
+    blue:   T,
+    alpha:  T,
+}
+
+impl<T> Pixel<T> {
+    pub fn new(red_value: T, green_value: T, blue_value: T, alpha_value: T) -> Pixel<T> {
+        Pixel { red: red_value, green: green_value, blue: blue_value, alpha: alpha_value }
+    }
+    pub fn set_red(&mut self, value: T) {
+        self.red = value;
+    }
+    pub fn get_red(&mut self) -> &T {
+        &self.red
+    }
+    pub fn set_green(&mut self, value: T) {
+        self.green = value;
+    }
+    pub fn get_green(&mut self) -> &T {
+        &self.green
+    }
+    pub fn set_blue(&mut self, value: T) {
+        self.blue = value;
+    }
+    pub fn get_blue(&mut self) -> &T {
+        &self.blue
+    }
+    pub fn set_alpha(&mut self, value: T) {
+        self.alpha = value;
+    }
+    pub fn get_alpha(&mut self) -> &T {
+        &self.alpha
+    }
+}
+
 pub fn i64_to_u8(value: i64) -> u8 {
     let cnv_value: u8;
     if value > 255i64 {
--- src/module/edge_detection.rs
+++ src/module/edge_detection.rs
@@ -3,7 +3,8 @@ use image::GenericImage;
 use image::DynamicImage;
 use image::Rgba;
 use crate::module::common::i64_to_u8;
-use crate::module::Pixel;
+use crate::module::common::Pixel;
+use crate::module::common::PixelKind;
 
 pub struct ImageManager {}
 
@@ -27,7 +28,7 @@ impl GoImageProc for ImageManager {
             for x in 0..width {
                 let pixel: Rgba<u8> = img.get_pixel(x, y);
 
-                let mut cal_pixel: Pixel<i64> = Pixel { red: 0, green: 0, blue: 0, alpha: pixel[3] as i64 };
+                let mut cal_pixel: Pixel<i64> = Pixel::new(0, 0, 0, pixel[PixelKind::Alpha as usize] as i64);
                 for y_mask in 0..mask_size {
                     for x_mask in 0..mask_size {
                         if x + x_mask > 0 &&
@@ -35,16 +36,39 @@ impl GoImageProc for ImageManager {
                         y + y_mask > 0 &&
                         y + y_mask < height {
                             let pixel2: Rgba<u8> = img.get_pixel(x + x_mask, y + y_mask);
-                            let pixel_data: Pixel<u8> = Pixel { red: pixel2[0], green: pixel2[1],  blue: pixel2[2], alpha: pixel2[3] };
-                            cal_pixel.red += pixel_data.red as i64 * mask[x_mask as usize][y_mask as usize] as i64;
-                            cal_pixel.green += pixel_data.green as i64 * mask[x_mask as usize][y_mask as usize] as i64;
-                            cal_pixel.blue += pixel_data.blue as i64 * mask[x_mask as usize][y_mask as usize] as i64;
+                            let mut pixel_data: Pixel<u8> = Pixel::new(
+                                pixel2[PixelKind::Red as usize],
+                                pixel2[PixelKind::Green as usize], 
+                                pixel2[PixelKind::Blue as usize],
+                                pixel2[PixelKind::Alpha as usize]
+                            );
+                            let set_value: i64 = cal_pixel.get_red() + pixel_data.get_red().clone() as i64 * mask[x_mask as usize][y_mask as usize] as i64;
+                            cal_pixel.set_red(set_value);
+                            let set_value: i64 = cal_pixel.get_green() + pixel_data.get_green().clone() as i64 * mask[x_mask as usize][y_mask as usize] as i64;
+                            cal_pixel.set_green(set_value);
+                            let set_value: i64 = cal_pixel.get_blue() + pixel_data.get_blue().clone() as i64 * mask[x_mask as usize][y_mask as usize] as i64;
+                            cal_pixel.set_blue(set_value);
                         }
                     }
                 }
-                let pixel_data: Pixel<u8> = Pixel { red: pixel[0], green: pixel[1],  blue: pixel[2], alpha: pixel[3] };
-                let cal_pixel: Pixel<u8> = Pixel { red: i64_to_u8(cal_pixel.red), green: i64_to_u8(cal_pixel.green),  blue: i64_to_u8(cal_pixel.blue), alpha: pixel_data.alpha };
-                let new_color = [cal_pixel.red, cal_pixel.green, cal_pixel.blue, cal_pixel.alpha];
+                let mut pixel_data: Pixel<u8> = Pixel::new(
+                    pixel[PixelKind::Red as usize],
+                    pixel[PixelKind::Green as usize], 
+                    pixel[PixelKind::Blue as usize],
+                    pixel[PixelKind::Alpha as usize]
+                );
+                let mut cal_pixel: Pixel<u8> = Pixel::new(
+                    i64_to_u8(cal_pixel.get_red().clone()),
+                    i64_to_u8(cal_pixel.get_green().clone()),
+                    i64_to_u8(cal_pixel.get_blue().clone()),
+                    pixel_data.get_alpha().clone()
+                );
+                let new_color = [
+                    cal_pixel.get_red().clone(),
+                    cal_pixel.get_green().clone(),
+                    cal_pixel.get_blue().clone(),
+                    cal_pixel.get_alpha().clone()
+                ];
                 let pixel: Rgba<u8> = Rgba(new_color);
                 img.put_pixel(x, y, pixel);
             }
